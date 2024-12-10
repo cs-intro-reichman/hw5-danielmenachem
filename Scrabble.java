d69e35935a1dca9655cfce690e737b79f27f0a0f
danielmenachem <daniellemenachem@gmail.com>
2024-12-11 01:57:38 +0200

    scrabble submission

--- Scrabble.java
+++ Scrabble.java
@@ -48,7 +48,12 @@ public class Scrabble {
 
 	// Checks if the given word is in the dictionary.
 	public static boolean isWordInDictionary(String word) {
-		//// Replace the following statement with your code
+		for (int i = 0; i < DICTIONARY.length; i++) {
+			String wordFromDic = DICTIONARY[i]; 
+			if (word.equals(wordFromDic)) {
+				return true; 
+			}
+		}
 		return false;
 	}
 	
@@ -56,16 +61,34 @@ public class Scrabble {
 	// If the length of the word equals the length of the hand, adds 50 points to the score.
 	// If the word includes the sequence "runi", adds 1000 points to the game.
 	public static int wordScore(String word) {
-		//// Replace the following statement with your code
-		return 0;
+		int score = 0; 
+		for (int i = 0; i < word.length(); i ++) {
+			char c = word.charAt(i); 
+			score += SCRABBLE_LETTER_VALUES[(int) c - 97]; 
+		}
+		score = score * word.length(); 
+		if (word.length() == HAND_SIZE) {
+			score += 50; 
+		}
+		if (MyString.subsetOf(word, "runi")) {
+			score += 1000; 
+		}
+		return score;
 	}
 
 	// Creates a random hand of length (HAND_SIZE - 2) and then inserts
 	// into it, at random indexes, the letters 'a' and 'e'
 	// (these two vowels make it easier for the user to construct words)
 	public static String createHand() {
-		//// Replace the following statement with your code
-		return null;
+		String letters = "abcdefghijklmnopqrstuvwxyz"; 
+		String hand = ""; 
+		for (int i = 0; i < HAND_SIZE - 2; i++) {
+			int randomIndex = (int) (Math.random() * 26); 
+			hand += letters.charAt(randomIndex); 
+		}
+		hand = MyString.insertRandomly('a', hand); 
+		hand = MyString.insertRandomly('e', hand); 
+		return hand;
 	}
 	
     // Runs a single hand in a Scrabble game. Each time the user enters a valid word:
@@ -85,9 +108,22 @@ public class Scrabble {
 			// non-whitespace characters. Whitespace is either space characters, or  
 			// end-of-line characters.
 			String input = in.readString();
-			//// Replace the following break statement with code
-			//// that completes the hand playing loop
-			break;
+			if (input.equals(".")) {
+				break; 
+			} else {
+				if (!MyString.subsetOf(input, hand)) {
+					System.out.println("Invalid word. Try again.");
+				} else {
+					if (!isWordInDictionary(input)) {
+						System.out.println("No such word in the dictionary. Try again.");
+					} else {
+						int wordScore = wordScore(input); 
+						score += wordScore; 
+						hand = MyString.remove(hand, input); 
+						System.out.println(input + " earned " + wordScore + " points. Score: " + score + " points."); 
+					}
+				}
+			}
 		}
 		if (hand.length() == 0) {
 	        System.out.println("Ran out of letters. Total score: " + score + " points");
@@ -110,9 +146,15 @@ public class Scrabble {
 			// Gets the user's input, which is all the characters entered by 
 			// the user until the user enter the ENTER character.
 			String input = in.readString();
-			//// Replace the following break statement with code
-			//// that completes the game playing loop
-			break;
+			if (input.equals("e")) {
+				break; 
+			}
+			if (input.equals("n")) {
+				playHand(createHand());
+			}
+			else {
+				System.out.println("ERROR");
+			}
 		}
 	}
 
@@ -122,7 +164,7 @@ public class Scrabble {
 		////testScrabbleScore();    
 		////testCreateHands();  
 		////testPlayHands();
-		////playGame();
+		///playGame();
 	}
 
 	public static void testBuildingTheDictionary() {
@@ -131,7 +173,7 @@ public class Scrabble {
 		for (int i = 0; i < 5; i++) {
 			System.out.println(DICTIONARY[i]);
 		}
-		System.out.println(isWordInDictionary("mango"));
+		System.out.println(isWordInDictionary("aah"));
 	}
 	
 	public static void testScrabbleScore() {
@@ -148,7 +190,7 @@ public class Scrabble {
 	}
 	public static void testPlayHands() {
 		init();
-		//playHand("ocostrza");
+	    //playHand("ocostrza");
 		//playHand("arbffip");
 		//playHand("aretiin");
 	}
